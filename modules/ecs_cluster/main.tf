dddda3c69faedff63b6cde458c5826dfb942d5e9
Vishav Deshwal <vishavdeshwal@MacBook-Pro-2.local>
2026-05-29 14:48:57 +0530

    small confiruation added in ecs module

--- modules/ecs_cluster/main.tf
+++ modules/ecs_cluster/main.tf
@@ -6,6 +6,12 @@ resource "aws_ecs_cluster" "cluster" {
     value = var.enable_container_insights ? "enabled" : "disabled"
   }
 
+  configuration {
+    execute_command_configuration {
+      logging = "DEFAULT"
+    }
+  }
+
   tags = {
     Name        = var.cluster_name
     Environment = var.environment
