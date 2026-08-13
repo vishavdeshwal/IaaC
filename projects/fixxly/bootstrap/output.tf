498e516d5eb3e8e68dbd261b9f460711d8fc8f5c
Vishav Deshwal <vishavdeshwal@gmail.com>
2026-08-13 11:10:40 +0530

    feat(fixxly): add bootstrap S3 bucket configuration

---
+++ projects/fixxly/bootstrap/output.tf
@@ -0,0 +1,4 @@
+output "s3_bucket_name" {
+  value       = aws_s3_bucket.state.id
+  description = "The name of the bootstrapped S3 bucket for storing remote state. Copy this into your environment backend blocks!"
+}
---
+++ projects/fixxly/bootstrap/variable.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  type        = string
+  description = "The target AWS region for the bootstrap backend resources."
+  default     = "ap-south-1"
+}
+
+variable "aws_profile" {
+  type        = string
+  description = "The AWS CLI profile to authenticate with."
+  default     = "fixxly"
+}
+
+variable "project" {
+  type        = string
+  description = "The project name prefix applied to the S3 bucket."
+  default     = "fixxly"
+}
